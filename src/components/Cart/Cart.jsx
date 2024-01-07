abc72302bee9d3747c70de645e5dda55c8e8f74a
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2024-01-07 11:37:44 +0300

    added logic on cartItems

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
 import style from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
-  const cartItems = (
-    <ul className={style.cart_items}>
-      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
-          <li>{item.name}</li>
-        ))}
-    </ul>
-  );
+	const cartCtx = useContext(CartContext);
 
-  return (
-    <Modal onClose={props.onClose}>
-      {cartItems}
-      <div className={style.total}>
-        <span>Total amount</span>
-        <span>35.36</span>
-      </div>
-      <div className={style.actions}>
-        <button className={style.button_alt} onClick={props.onClose}>Close</button>
-        <button className={style.button}>Order</button>
-      </div>
-    </Modal>
-  );
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
+
+	const cartItemRemoveHandler = (id) => {};
+
+	const cartItemAddHandler = (item) => {};
+
+	const cartItems = (
+		<ul className={style.cart_items}>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
+			))}
+		</ul>
+	);
+
+	return (
+		<Modal onClose={props.onClose}>
+			{cartItems}
+			<div className={style.total}>
+				<span>Total amount</span>
+				<span>{totalAmount}</span>
+			</div>
+			<div className={style.actions}>
+				<button className={style.button_alt} onClick={props.onClose}>
+					Close
+				</button>
+				{hasItems && <button className={style.button}>Order</button>}
+			</div>
+		</Modal>
+	);
 };
 
 export default Cart;
---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,23 @@
+import style from './CartItem.module.css';
+
+const CartItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={style['cart-item']}>
+      <div>
+        <h2>{props.name}</h2>
+        <div className={style.summary}>
+          <span className={style.price}>{price}</span>
+          <span className={style.amount}>x {props.amount}</span>
+        </div>
+      </div>
+      <div className={style.actions}>
+        <button onClick={props.onRemove}>−</button>
+        <button onClick={props.onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
