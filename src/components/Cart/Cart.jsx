501fd54f38d61efceae434d779d5784af0a82b13
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2024-01-07 23:45:57 +0300

    added logic on remove and add items in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,9 +10,14 @@ const Cart = (props) => {
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 	const hasItems = cartCtx.items.length > 0;
 
-	const cartItemRemoveHandler = (id) => {};
+	const cartItemRemoveHandler = (id) => {
+    // console.log(`id: `+ id);
+    cartCtx.removeItem(id);
+  };
 
-	const cartItemAddHandler = (item) => {};
+	const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1})
+  };
 
 	const cartItems = (
 		<ul className={style.cart_items}>
--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -2,49 +2,89 @@ import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
 const defaultCartState = {
-  items: [],
-  totalAmount: 0,
+	items: [],
+	totalAmount: 0,
 };
 
 const cartReducer = (state, action) => {
-  if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amount;
-    return {
-      items: updatedItems,
-      totalAmount: updatedTotalAmount,
-    };
-  }
-  return defaultCartState;
+	if (action.type === "ADD") {
+    console.log("ADD");
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.item.id
+		);
+		const existingCartItem = state.items[existingCartItemIndex];
+		let updatedItems;
+
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			updatedItems = state.items.concat(action.item);
+		}
+
+		const updatedTotalAmount =
+			state.totalAmount + action.item.price * action.item.amount;
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+	if (action === "REMOVE") {
+    console.log("hui");
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+    console.log([action.id])
+		const existingItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingItem.price;
+		let updatedItems;
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id !== action.id);
+		} else {
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+	return defaultCartState;
 };
 
 const CartProvider = (props) => {
-  const [cartState, dispatchCartAction] = useReducer(
-    cartReducer,
-    defaultCartState
-  );
+	const [cartState, dispatchCartAction] = useReducer(
+		cartReducer,
+		defaultCartState
+	);
 
-  const addItemToCardHandler = (item) => {
-    dispatchCartAction({ type: "ADD", item: item });
-  };
+	const addItemToCardHandler = (item) => {
+		dispatchCartAction({ type: "ADD", item: item });
+	};
 
-  const removeItemToCardHandler = (id) => {
-    dispatchCartAction({ type: "REMOVE", id: id });
-  };
+	const removeItemToCardHandler = (id) => {
+		dispatchCartAction({ type: "REMOVE", id: id });
+	};
 
-  const cartContext = {
-    items: cartState.items,
-    totalAmount: cartState.totalAmount,
-    addItem: addItemToCardHandler,
-    removeItem: removeItemToCardHandler,
-  };
+	const cartContext = {
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
+		addItem: addItemToCardHandler,
+		removeItem: removeItemToCardHandler,
+	};
 
-  return (
-    <CartContext.Provider value={cartContext}>
-      {props.children}
-    </CartContext.Provider>
-  );
+	return (
+		<CartContext.Provider value={cartContext}>
+			{props.children}
+		</CartContext.Provider>
+	);
 };
 
 export default CartProvider;
